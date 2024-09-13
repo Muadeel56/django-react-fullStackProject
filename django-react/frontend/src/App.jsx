5b79484f57fd520a89a6937109c00b2c46004753
Muadeel56 <muadeel59@gmail.com>
2024-09-13 16:09:59 +0500

    Setting Up Frontend

---
+++ django-react/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  
+
+  return (
+    <section className="w-full h-full bg-black">
+     <h1 className="text-white">I am Muhammad Adeel</h1>
+    </section>
+  )
+}
+
+export default App
